cccc8ef44d087d5e2de5f6ac95710e9ca91b1cf5
jiayingz0220 <51219684+jiayingz0220@users.noreply.github.com>
2019-05-31 13:50:40 +0800

    Add files via upload

--- labs/lab1/alternating.cpp
+++ labs/lab1/alternating.cpp
@@ -5,11 +5,46 @@ using namespace std;
 
 bool is_alternating(int num, int base) {
     // TODO: Your implementation here
-    return false;
+    int baseb[50];
+    int n1, i, j, index, k;
+    n1 = num; i = 0; index = 0;
+    for (k = 0; k<50; k++){
+        baseb[k] = 0;
+    }
+    while (n1 > 0){
+        baseb[i] = n1%base;
+        n1/=base;
+        i++;
+    }
+    if (i == 1){
+        return true;
+    }
+    else{
+        for (j = 1; j < i; j++){
+            if (abs((baseb[j] - baseb[j - 1]))%2 == 1){
+                index = 1;
+            }
+            else{
+                index = 0;
+                break;
+            }
+        }
+        if (index == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
 }
 
 void test_alternating() {
-    assert(!is_alternating(24, 10));  // 24 is not an alternating number in base 10
+    assert(!is_alternating(24, 10));
+    assert(is_alternating(21, 2));
+
+
+    // 24 is not an alternating number in base 10
     // TODO: Add more test cases
     cout << "Alternating number tests passed!" << endl;
 }
--- labs/lab1/duffinian.cpp
+++ labs/lab1/duffinian.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 using namespace std;
 
 bool is_duffinian(int num) {
     // TODO: Your implementation here
+    int i, j, h, k, sum, index, x;
+    int div[100] = {};
+    for (x =2;x<100; x++ ){
+        div[x] = 0;
+    }
+    div[0] = 1; div[1] = num;
+    j = 2; sum = 0; index = 0;
+    for (i = 2; i<=sqrt(num); i++) {
+        if (num % i == 0) {
+            div[j] = i;
+            if (i != num / i) {
+                div[j + 1] = num / i;
+                j += 2;
+            }
+            else {
+                j++;
+            }
+        }
+    }
+    for (k = 0; k < j; k++) {
+        sum += div[k];
+    }
+
+    for (h = 2; h <= num; h++){
+         if (num%h == 0 && sum%h ==0) {
+             index = 0;
+             break;
+         }
+         else {
+             index = 1;
+         }
+    }
+    if (index == 0){
     return false;
+    }
+    else{
+        return true;
+    }
 }
+
 
 void test_duffinian() {
     assert(!is_duffinian(6));  // 6 is not a Duffinian number
+    assert(is_duffinian(35));
     // TODO: Add more test cases
     cout << "Duffinian number tests passed!" << endl;
 }
